89fa32176086512cd7138232950ca0486d0fa8b4
MrRobot747 <eabdihamitov@gmail.com>
2025-02-08 12:18:12 +0500

    Delete LAB 3/Python Function/4.py

--- LAB 3/Python Function/4.py
+++
@@ -1,6 +0,0 @@
-def filter_prime(numbers):
-    return [n for n in numbers if n > 1 and all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))]
-
-
-numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
-print("Prime numbers:", filter_prime(numbers))
